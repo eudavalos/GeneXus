a52cf8cd3f1c8719d1775c5dbd1d7357bf271aba
eudavalos <eudavalos91@gmail.com>
2023-12-01 18:26:59 -0300

    Prueba 13

--- tomcat/bin/setenv.sh
+++ tomcat/bin/setenv.sh
@@ -3,6 +3,7 @@ echo "GeneXus - Setting custom environment variables for Tomcat"
 export CATALINA_OPTS="$CATALINA_OPTS -XX:HeapDumpPath=/logs/tomcat/java_heapdump_pid_%p.log"
 export CATALINA_OPTS="$CATALINA_OPTS -XX:+HeapDumpOnOutOfMemoryError"
 export CATALINA_OPTS="$CATALINA_OPTS -Djava.awt.headless=true"
+set CATALINA_OPTS="-Xms4096m -Xmx8192m"
 
 if [ -r "$CATALINA_BASE/bin/otelenv.sh" ]; then
   . "$CATALINA_BASE/bin/otelenv.sh"
