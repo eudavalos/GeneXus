f96f42134f0f624b8571f8a8abb281d6e17b0783
eudavalos <eudavalos91@gmail.com>
2023-12-02 14:00:57 -0300

    Prueba 25

--- tomcat/bin/setenv.sh
+++ tomcat/bin/setenv.sh
@@ -1,13 +1,13 @@
 echo "GeneXus - Setting custom environment variables for Tomcat"
 
-export CATALINA_OPTS="$CATALINA_OPTS -XX:HeapDumpPath=/logs/tomcat/java_heapdump_pid_%p.log"
-export CATALINA_OPTS="$CATALINA_OPTS -XX:+HeapDumpOnOutOfMemoryError"
-export CATALINA_OPTS="$CATALINA_OPTS -Djava.awt.headless=true"
-export CATALINA_OPTS="-Xms128m -Xmx256m"
+#export CATALINA_OPTS="$CATALINA_OPTS -XX:HeapDumpPath=/logs/tomcat/java_heapdump_pid_%p.log"
+#export CATALINA_OPTS="$CATALINA_OPTS -XX:+HeapDumpOnOutOfMemoryError"
+#export CATALINA_OPTS="$CATALINA_OPTS -Djava.awt.headless=true"
+export CATALINA_OPTS="-Xms256m -Xmx312m"
 # Configuración del recolector de basura (G1GC)
-export CATALINA_OPTS="$CATALINA_OPTS -XX:+UseG1GC"
-export CATALINA_OPTS="$CATALINA_OPTS -XX:MaxGCPauseMillis=200"
-export CATALINA_OPTS="$CATALINA_OPTS -XX:G1ReservePercent=20"
+#export CATALINA_OPTS="$CATALINA_OPTS -XX:+UseG1GC"
+#export CATALINA_OPTS="$CATALINA_OPTS -XX:MaxGCPauseMillis=200"
+#export CATALINA_OPTS="$CATALINA_OPTS -XX:G1ReservePercent=20"
 
 if [ -r "$CATALINA_BASE/bin/otelenv.sh" ]; then
   . "$CATALINA_BASE/bin/otelenv.sh"
